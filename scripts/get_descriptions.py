488103d8908d2b8431acca0080d71a1ad6a57ac3
Peter Mocarski <pmm248@cornell.edu>
2018-05-08 01:45:20 -0400

    Implemented basic front end

---
+++ scripts/get_descriptions.py
@@ -0,0 +1,22 @@
+import wikipedia
+import json
+import sys
+
+descriptions = {}
+
+with open('eco.json') as file:
+	openings = json.load(file)
+	for opening in openings:
+		opening_name = opening['n']
+		print(opening_name)
+		if ":" in opening_name:
+			continue
+		sys.stdout.flush()
+		try:
+			opening_description = wikipedia.summary(opening_name, sentences=6)
+			descriptions[opening_name] = opening_description
+		except:
+			pass
+
+with open('descriptions.json', 'w') as file:
+	json.dump(descriptions, file)
