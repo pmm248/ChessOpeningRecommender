e33d1a9958a1164d406fd6dd370ab9cb11ba2b55
Peter Mocarski <pmm248@cornell.edu>
2018-05-08 02:15:20 -0400

    Now returns random opening

--- js/recommender.js
+++ js/recommender.js
@@ -1,5 +1,18 @@
+// get descriptions JSON
+var xhReq = new XMLHttpRequest();
+xhReq.open("GET", "http://pmocarski.com/ChessOpeningRecommender/data/descriptions.json", false);
+xhReq.send(null);
+var descriptions_map = JSON.parse(xhReq.responseText);
+var descriptions = []
+for (var key in descriptions_map){
+	if (descriptions_map.hasOwnProperty(key)){
+		descriptions.push( [key, descriptions_map[key]]);
+	}
+}
+
 $(document).ready(function(){
     $("#submitButton").click(function(){
-        alert("Danish Gambit");
+		var result = descriptions[Math.floor(Math.random()*descriptions.length)];
+        alert(result[0]);
     });
 });
